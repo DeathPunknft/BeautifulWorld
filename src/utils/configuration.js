850d71e9aa3663428d2a4c80f53d2abb3cb67b72
DeathPunknft <108284347+DeathPunknft@users.noreply.github.com>
2022-07-16 15:42:55 +0800

    Add files via upload

---
+++ src/utils/configuration.js
@@ -0,0 +1,17 @@
+// Set smart contract based on chain id
+export const CONTRACTADDRESS = {
+    1: "0xB3025E6083a8c79537C9356e9284A8227581aF1a",
+    4: "",
+};
+
+// Set the price of your NFT
+export const PRICE = 0.006;
+
+// Set the max amount of your NFT collection
+export const MAX_AMOUNT = 10000;
+
+// Set the max batch size per wallet 
+export const MAX_BATCH_SIZE = 5;
+
+// Set the opesnea url 
+export const OPENSEA_URL = "https://opensea.io/collection/BeautifulWorld-NFT"
